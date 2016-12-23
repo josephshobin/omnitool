5770b8e3dee0f75b2898bca126971df3b7629de6
Todd Owen <todd.owen@cba.com.au>
2016-12-23 16:01:32 +1100

    Automatic version update (#39)

--- project/build.scala
+++ project/build.scala
@@ -28,7 +28,7 @@ object build extends Build {
       updateOptions := updateOptions.value.withCachedResolution(true)
     )
 
-  val omniaTestVersion = "2.4.5-20161216000205-9100ede"
+  val omniaTestVersion = "2.4.6-20161223030915-7f40c35"
 
   lazy val root =
     Project(
